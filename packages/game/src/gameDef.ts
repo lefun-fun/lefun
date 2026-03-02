db7dbe4673b03760c04f5a3c390881a988229e1c
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2026-03-02 03:17:11 +0000

    Add `ts` to `executeNow`
    This enables optimistic UI for things that require the current
    timestamp. An estimated "server timestamp" will be used.

--- packages/game/src/gameDef.ts
+++ packages/game/src/gameDef.ts
@@ -147,8 +147,6 @@ export type MoveSideEffects<
   logMatchStat: LogMatchStat<MS>;
 };
 
-// TODO Add `ts` in the options here
-// https://github.com/lefun-fun/lefun/issues/52
 type ExecuteNowOptions<
   GS extends GameStateBase,
   P,
@@ -162,6 +160,7 @@ type ExecuteNowOptions<
   // Assume that the game developer has defined the playerboard if they're using it.
   playerboard: GS["PB"];
   payload: P;
+  ts: number;
   _: MoveSideEffects<PMT, BMT, PS, MS>;
 } & MoveSideEffects<PMT, BMT, PS, MS>;
 
