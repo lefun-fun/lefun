4a7de23956145a6c303f33d9d736d9ea29d33205
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2026-05-17 20:56:31 +0000

    Make the test game work

--- games/game1-v2.7.0/vite.config.ts
+++ games/game1-v2.7.0/vite.config.ts
@@ -1,14 +1,12 @@
-import { lingui } from "@lingui/vite-plugin";
+import { lingui, linguiTransformerBabelPreset } from "@lingui/vite-plugin";
+import babel from "@rolldown/plugin-babel";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: ["@lingui/babel-plugin-lingui-macro"],
-      },
-    }),
+    react(),
+    babel({ presets: [linguiTransformerBabelPreset()] }),
     lingui(),
   ],
   resolve: {
